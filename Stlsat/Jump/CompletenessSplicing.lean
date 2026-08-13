05f194f8de7a3c4564c00908701b1378e39ba7fc
Michele Chiari <michele.chiari@ait.ac.at>
2026-08-13 16:51:48 +0200

    Update license declaration in headers

--- Stlsat/Jump/CompletenessSplicing.lean
+++ Stlsat/Jump/CompletenessSplicing.lean
@@ -1,6 +1,6 @@
 /-
 Copyright (c) 2026 Michele Chiari. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
+Released under the MIT license as described in the file LICENSE.
 Authors: Michele Chiari
 -/
 import Stlsat.Basic.Completeness
